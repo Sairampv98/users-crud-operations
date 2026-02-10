9bd52a0be0333633ff71af7c3220d256f1681fb6
Sai Ram <97302336+Sairampv98@users.noreply.github.com>
2026-02-10 09:00:39 +0530

    working-repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,9 @@
 import "./App.css";
-// import JSX from "./components/complexElement";
-// import FuncComponent from "./components/StatelessFuctionalComponent";
-// import ReactComponent from "./components/ReactComponent";
-// import ParentComponent from "./components/ParentComponent";
-// import RComponent from "./components/FreeCodeCamp";
 import { Toaster } from "react-hot-toast";
-import User1 from "./components/getuser/User";
-// import Add from "./components/adduser/Add";
+import User from "./components/User.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Edit from "./components/updateuser/Edit";
-import Register from "./components/Register";
-import Create from "./components/adduser/Add";
+import Edit from "./components/Edit.jsx";
+import Create from "./components/Add.jsx";
 function App() {
   return (
     <>
@@ -18,20 +11,12 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-          <Route path="/register" element={<Register />} />,
-            <Route path="/" element={<User1 />} />,
+            <Route path="/" element={<User />} />,
             <Route path="/add" element={<Create />} />,
             <Route path="/edit/:id" element={<Edit />} />,
           </Routes>
         </div>
       </Router>
-
-      {/* <ReactComponent />
-      <ParentComponent />
-      <RComponent />
-
-      <JSX />
-      <FuncComponent /> */}
     </>
   );
 }
--- client/src/services/authService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const register =async(userData)=>{
-    return await axios.post(`http://localhost:3000/api/register`,userData);
-
-};
